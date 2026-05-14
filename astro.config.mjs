0e5c97aa1325a0d583c65b02e6c2801955d22374
Ewa Ramus <you@gmail.com>
2026-05-14 22:19:36 +0200

    feat: add Tailwind visual system

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://ekaramus.github.io",
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
